6220604362b20495a28574b581a6e7a458e692a2
Stoycho Levkov <stlevkov@gmail.com>
2024-12-26 14:29:42 +0200

    Externalized reusable code in util file

--- display_tooltip.lua
+++ display_tooltip.lua
@@ -1,132 +1,7 @@
 -- /console scriptErrors 1 (to see lua errors) (ingame)
 
 -- Global variable for storing chat message state
-chatObj = { chatMsgFired = false, currentItem = nil }
-
-local englishClass = select(2, UnitClass("player"))
-
--- Function to process the class specialization based on talent points
-local function processClassSpecialization()
-    local numTabs = GetNumTalentTabs()
-    local talents = { 0, 0, 0 }
-
-    for t = 1, numTabs do
-        local numTalents = GetNumTalents(t)
-        local calculateTalents = 0
-        for i = 1, numTalents do
-            nameTalent, icon, tier, column, currRank, maxRank = GetTalentInfo(t, i)
-            calculateTalents = calculateTalents + currRank
-        end
-        talents[t] = calculateTalents
-    end
-    return talents
-end
-
--- Function to determine the specialization based on talents and class
-local function getClassRole()
-    local classRole = processClassSpecialization()
-
-    if englishClass == "ROGUE" then
-        if classRole[3] > classRole[1] and classRole[3] > classRole[2] then
-            return "Subtlety"
-        elseif classRole[2] > classRole[1] and classRole[2] > classRole[3] then
-            return "Assassination"
-        else
-            return "Combat"
-        end
-    elseif englishClass == "PALADIN" then
-        if classRole[3] > classRole[1] and classRole[3] > classRole[2] then
-            return "Retribution"
-        elseif classRole[2] > classRole[1] and classRole[2] > classRole[3] then
-            return "Protection"
-        else
-            return "Holy"
-        end
-    elseif englishClass == "WARRIOR" then
-        if classRole[2] > classRole[1] and classRole[2] > classRole[3] then
-            return "Fury"
-        elseif classRole[3] > classRole[1] and classRole[3] > classRole[2] then
-            return "Protection"
-        else
-            return "Arms"
-        end
-    elseif englishClass == "SHAMAN" then
-        if classRole[2] > classRole[1] and classRole[2] > classRole[3] then
-            return "Enhancement"
-        elseif classRole[3] > classRole[1] and classRole[3] > classRole[2] then
-            return "Restoration"
-        else
-            return "Elemental"
-        end
-    elseif englishClass == "WARLOCK" then
-        if classRole[1] > classRole[2] and classRole[1] > classRole[3] then
-            return "Affliction"
-        elseif classRole[2] > classRole[1] and classRole[2] > classRole[3] then
-            return "Demonology"
-        else
-            return "Destruction"
-        end
-    elseif englishClass == "HUNTER" then
-        if classRole[1] > classRole[2] and classRole[1] > classRole[3] then
-            return "Beast Mastery"
-        elseif classRole[2] > classRole[1] and classRole[2] > classRole[3] then
-            return "Marksmanship"
-        else
-            return "Survival"
-        end
-    elseif englishClass == "DRUID" then
-        if classRole[2] > classRole[1] and classRole[2] > classRole[3] then
-            if classRole[3] > 11 then
-                return "FeralDPS"
-            else
-                return "FeralTank"
-            end
-        elseif classRole[3] > classRole[1] and classRole[3] > classRole[2] then
-            return "Restoration"
-        else
-            return "Balance"
-        end
-    elseif englishClass == "PRIEST" then
-        if classRole[1] > classRole[2] and classRole[1] > classRole[3] then
-            return "Shadow"
-        elseif classRole[2] > classRole[1] and classRole[2] > classRole[3] then
-            return "Holy"
-        else
-            return "Discipline"
-        end
-    elseif englishClass == "MAGE" then
-        if classRole[1] > classRole[2] and classRole[1] > classRole[3] then
-            return "Arcane"
-        elseif classRole[2] > classRole[1] and classRole[2] > classRole[3] then
-            return "Fire"
-        else
-            return "Frost"
-        end
-    elseif englishClass == "BIS_LIST_DEATHKNIGHT" then
-        if classRole[1] > classRole[2] and classRole[1] > classRole[3] then
-            return "Blood"
-        elseif classRole[2] > classRole[1] and classRole[2] > classRole[3] then
-            return "Frost"
-        else
-            return "Unholy"
-        end
-    else
-        print("BIS will not work for this Class")
-    end
-end
-
--- Function to load the appropriate BIS list based on class and spec
-local function LoadBISListForSpec(specName)
-    local specBIS = _G["BIS_LIST_" .. englishClass]
-
-    if specBIS then
-        -- Get BIS and Pre-BIS for the current spec
-        local classSpecBIS = specBIS[specName] or {BIS = {}, PreBIS = {}}  -- Default to empty if spec not found
-        return classSpecBIS
-    end
-
-    return {BIS = {}, PreBIS = {}} -- Default empty lists if BIS list not found
-end
+local chatObj = { chatMsgFired = false, currentItem = nil }
 
 -- Function to add static text with BIS and Pre-BIS item links to the tooltip for the HeadSlot item
 local function AddStaticTextToTooltip(tooltip)
@@ -163,11 +38,11 @@ local function AddStaticTextToTooltip(tooltip)
     end
 
     -- Determine the class and specialization
-    local specName = getClassRole()
+    local specName = GetClassRoleBisCore()
 
     -- Load BIS and Pre-BIS lists for the current spec
-    local BIS_LIST = LoadBISListForSpec(specName).BIS
-    local PRE_BIS_LIST = LoadBISListForSpec(specName).PreBIS
+    local BIS_LIST = LoadBISListForSpecBisCore(specName).BIS
+    local PRE_BIS_LIST = LoadBISListForSpecBisCore(specName).PreBIS
 
     -- Ensure BIS and Pre-BIS lists are available
     if not BIS_LIST or not PRE_BIS_LIST then
@@ -237,25 +112,6 @@ local function AddStaticTextToTooltip(tooltip)
     -- Show the tooltip
     tooltip:Show()
 end
-
--- -- Handle item info loading if the item is not yet cached
--- local function OnItemInfoReceived(itemID)
---     print("Item info received for itemID: " .. itemID)
---     -- Re-fetch item details after the item info is loaded
---     local randomItemName, randomItemLink = GetItemInfo(itemID)
---     if randomItemLink then
---         -- Update tooltips with the loaded item info
---         GameTooltip:ClearLines()
---         AddStaticTextToTooltip(GameTooltip)
---     end
--- end
-
--- -- Hook into the GET_ITEM_INFO_RECEIVED event to handle item info once loaded
--- local frame = CreateFrame("Frame")
--- frame:RegisterEvent("GET_ITEM_INFO_RECEIVED")
--- frame:SetScript("OnEvent", function(self, event, itemID)
---     OnItemInfoReceived(itemID)
--- end)
 
 -- Hook GameTooltip to show static text with BIS and Pre-BIS item links for the HeadSlot item
 GameTooltip:HookScript("OnTooltipSetItem", function(self)
