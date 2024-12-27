e1b6d70336e30275ff60938430f68aa53e971aec
Stoycho Levkov <stlevkov@gmail.com>
2024-12-27 15:14:18 +0200

    Adding Shaman pre-bis list;

--- shaman.lua
+++ shaman.lua
@@ -16,12 +16,30 @@ BIS_LIST_SHAMAN = {
             { slot = "Finger1Slot", itemID = 50400, source = "ICC Vendor" }, -- Ashen Band of Endless Wisdom
             { slot = "Trinket0Slot", itemID = 50366, source = "Gunship Battle 25HC" }, -- Althor's Abacus
             { slot = "Trinket1Slot", itemID = 54589, source = "Halion 25HC" }, -- Glowing Twilight Scale
-            { slot = "MainHandSlot", itemID = 46017, source = "Ulduar Quest Line" }, -- Val'anyr, Hammer of Ancient Kings
+            { slot = "MainHandSlot", itemID = 50734, source = "The Lich King 25HC" }, -- Royal Scepter of Terenas II
+            { slot = "MainHand0Slot", itemID = 46017, source = "Ulduar Quest Line" }, -- Val'anyr, Hammer of Ancient Kings
             { slot = "SecondaryHandSlot", itemID = 50616, source = "Marrowgar 25HC" }, -- Bulwark of Smouldering Steel
-            { slot = "RangedSlot", itemID = 50458, source = "Frost Emblem Vendor" } -- Bizuri's Totem of Shattered Ice
+            { slot = "RangedSlot", itemID = 50464, source = "Vendor Frost Emblem" } -- Totem of the Surging Sea
         },
         PreBIS = {
-            { slot = "HeadSlot", itemID = 51192, source = "Vendor" }, -- Sanctified Frost Witch's Faceguard (Normal Mark)
+            { slot = "HeadSlot", itemID = 51192, source = "Vendor Normal Mark" }, -- Sanctified Frost Witch's Faceguard
+            { slot = "NeckSlot", itemID = 50182, source = "Blood Queen Lana'thel 25 or Action House" }, -- Blood Queen's Crimson Choker
+            { slot = "ShoulderSlot", itemID = 51194, source = "Vendor Normal Mark" }, -- Sanctified Frost Witch's Spaulders
+            { slot = "BackSlot", itemID = 53489, source = "Halion 25" }, -- Cloak of Burning Dusk
+            { slot = "ChestSlot", itemID = 51190, source = "Vendor Normal Mark" }, -- Sanctified Frost Witch's Tunic
+            { slot = "WristSlot", itemID = 50030, source = "Rotface 25" }, -- Bloodsunder's Bracers
+            { slot = "HandsSlot", itemID = 50064, source = "Festergut 25" }, -- Unclean Surgical Gloves
+            { slot = "WaistSlot", itemID = 53488, source = "Halion 25" }, -- Split Shape Belt
+            { slot = "LegsSlot", itemID = 51193, source = "Vendor Normal Mark" }, -- Sanctified Frost Witch's Legguards
+            { slot = "FeetSlot", itemID = 50062, source = "Festergut 25" }, -- Plague Scientist's Boots
+            { slot = "Finger0Slot", itemID = 50008, source = "Gunship Battle 25" }, -- Ring of Rapid Ascent
+            { slot = "Finger1Slot", itemID = 50400, source = "ICC Vendor" }, -- Ashen Band of Endless Wisdom
+            { slot = "Trinket0Slot", itemID = 50359, source = "Gunship Battle 25" }, -- Althor's Abacus
+            { slot = "Trinket1Slot", itemID = 54573, source = "Halion 25" }, -- Glowing Twilight Scale
+            { slot = "MainHandSlot", itemID = 50428, source = "The Lich King 25" }, -- Royal Scepter of Terenas II
+            { slot = "MainHand0Slot", itemID = 50685, source = "Rotface 25HC" }, -- Trauma
+            { slot = "SecondaryHandSlot", itemID = 49976, source = "Lord Marrowgar 25" }, -- Bulwark of Smouldering Steel
+            { slot = "RangedSlot", itemID = 47665, source = "Vendor Triumph Emblem" } -- Totem of Calming Tides
         }
     },
     -- Enhancement Shaman BIS
